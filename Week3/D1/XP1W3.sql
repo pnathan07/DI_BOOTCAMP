86a80ea3f5c3f58c3929a8ad5b26964d834979f8
Nathan <nathan.part34123@gmail.com>
2024-06-23 15:36:18 +0300

    finish xp1w3

---
+++ Week3/D1/XP1W3.sql
@@ -0,0 +1,43 @@
+--1 Add the following items to the items table : --
+--create table itmes_list (
+--	id serial primary key,
+--	price float,
+--	items varchar(20)
+--);
+
+--insert into itms_list (price, items)
+
+--values
+--(100,'small desk'),
+--(300, 'larg desk'),
+--(80, 'fan');
+
+--select * from items_list 
+
+--Add 5 n ew customer
+
+--create table customers_table (
+--id serial primary key,
+  --"First Name" varchar (20),
+  --"Last Name" (20)
+--);
+
+--values
+--Greg - Jones
+--('Sandra', 'Jones')
+--('Scott', 'Scott')
+--('Trevor' 'Green')
+--('Melanie', 'Johnson')
+--);
+
+--select * from customer_table
+
+--select* from items_list
+--select* from items_list where price > 80
+--select* from items_list where price < 300
+
+--select "Last Name" from customers_table where "Last Name" = 'Smith'
+--select from customer_table where "Last Name" = 'Jones'
+--select from customer_table where "First Name" != 'Scott'
+
+
